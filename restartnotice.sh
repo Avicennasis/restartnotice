619aa0154b61ad4a0bd4367d4acf5220040618c1
Avic <Avicennasis@gmail.com>
2017-03-27 22:54:52 -0400

    Update restartnotice.sh
    Double quotes to prevent globbing and word splitting, using $(..) instead of legacy `..`.

--- restartnotice.sh
+++ restartnotice.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "What did you break now? Server" `hostname` " restarted as of $(date '+%Y %b %d %H:%M:%S')" | mail -s "System Restart" USERNAME@gmail.com
+echo "What did you break now? Server" "$(hostname)" " restarted as of $(date '+%Y %b %d %H:%M:%S')" | mail -s "System Restart" USERNAME@gmail.com
